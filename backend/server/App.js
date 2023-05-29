ecfdda7494448c8ccade728dbc616cadf4b32031
kamcod <mkamran4786@gmail.com>
2023-05-29 08:22:56 +0500

    start node js server in app

--- backend/server/App.js
+++ backend/server/App.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`server is listening at port ${port}`)
+});
